933e6b04a81ef642c288316b77358cb8395e66fa
Dragomir Marinov <a11824886@univie.ac.at>
2024-06-12 20:03:30 +0200

    reverting before merge

--- src/main/java/org/omilab/portal_service/PortalServiceApplication.java
+++ src/main/java/org/omilab/portal_service/PortalServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PortalServiceApplication extends SpringBootServletInitializer {
@@ -19,20 +17,4 @@ public class PortalServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PortalServiceApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Apply CORS settings to all endpoints under `/portal_service/api/**`
-                registry.addMapping("/portal_service/api/**")  // Adjust the pattern to match all endpoints
-                        .allowedOrigins("http://localhost:8080")  // Allow all requests from this origin
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // Specify the HTTP methods allowed
-                        .allowedHeaders("*")  // Allow all headers
-                        .allowCredentials(true);  // Allow credentials like cookies, authorization headers, etc.
-            }
-        };
-    }
-    
 }
--- src/main/java/org/omilab/portal_service/service/StatsService.java
+++ src/main/java/org/omilab/portal_service/service/StatsService.java
@@ -1,91 +1,90 @@
-//package org.omilab.portal_service.service;
-//
-//
-//import org.joda.time.DateTime;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//@RequestMapping("/stats")
-//public class StatsService {
-//
-//    @GetMapping("/spending")
-//    public String searchAttractions(
-//            @RequestParam(value = "districts", required = false) String districtStr,
-//            @RequestParam(value = "ageRange", required = false) String ageRangeStr,
-//            @RequestParam(value = "gender", required = false) String gender,
-//            @RequestParam(value = "timeRange", required = false) String timeRangeStr) {
-//
-//        List<Integer> districts = null;
-//        if (districtStr != null && !districtStr.isEmpty()) {
-//            districts = Arrays.stream(districtStr.split(","))
-//                    .map(Integer::parseInt)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        List<DateTime> timeRange = null;
-//        if (timeRangeStr != null && !timeRangeStr.isEmpty()) {
-//            timeRange = Arrays.stream(timeRangeStr.split(","))
-//                    .map(DateTime::parse)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        List<Integer> ageRange = null;
-//        if (ageRangeStr != null && !ageRangeStr.isEmpty()) {
-//            ageRange = Arrays.stream(ageRangeStr.split(","))
-//                    .map(Integer::parseInt)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        StringBuilder sb = new StringBuilder("Attractions filtered by: \n");
-//        if (districts != null) sb.append("Districts: ").append(districts).append("\n");
-//        if (ageRange != null) sb.append("Age Range: ").append(ageRange).append("\n");
-//        if (gender != null) sb.append("Gender: ").append(gender).append("\n");
-//        if (timeRange != null) sb.append("Time Range: ").append(timeRange).append("\n");
-//
-//        return sb.toString();
-//    }
-//
-//    @GetMapping("/visits")
-//    public String searchEvents(
-//            @RequestParam(value = "districts", required = false) String districtStr,
-//            @RequestParam(value = "gender", required = false) String gender,
-//            @RequestParam(value = "ageRange", required = false) String ageRangeStr,
-//            @RequestParam(value = "timeRange", required = false) String timeRangeStr) {
-//
-//        List<Integer> districts = null;
-//        if (districtStr != null && !districtStr.isEmpty()) {
-//            districts = Arrays.stream(districtStr.split(","))
-//                    .map(Integer::parseInt)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        List<DateTime> timeRange = null;
-//        if (timeRangeStr != null && !timeRangeStr.isEmpty()) {
-//            timeRange = Arrays.stream(timeRangeStr.split(","))
-//                    .map(DateTime::parse)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        List<Integer> ageRange = null;
-//        if (ageRangeStr != null && !ageRangeStr.isEmpty()) {
-//            ageRange = Arrays.stream(ageRangeStr.split(","))
-//                    .map(Integer::parseInt)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        StringBuilder sb = new StringBuilder("Attractions filtered by: \n");
-//        if (districts != null) sb.append("Districts: ").append(districts).append("\n");
-//        if (ageRange != null) sb.append("Age Range: ").append(ageRange).append("\n");
-//        if (gender != null) sb.append("Gender: ").append(gender).append("\n");
-//        if (timeRange != null) sb.append("Time Range: ").append(timeRange).append("\n");
-//
-//        return sb.toString();
-//    }
-//}
+package org.omilab.portal_service.service;
+
+import org.joda.time.DateTime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/stats")
+public class StatsService {
+
+    @GetMapping("/spending")
+    public String searchAttractions(
+            @RequestParam(value = "districts", required = false) String districtStr,
+            @RequestParam(value = "ageRange", required = false) String ageRangeStr,
+            @RequestParam(value = "gender", required = false) String gender,
+            @RequestParam(value = "timeRange", required = false) String timeRangeStr) {
+
+        List<Integer> districts = null;
+        if (districtStr != null && !districtStr.isEmpty()) {
+            districts = Arrays.stream(districtStr.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+
+        List<DateTime> timeRange = null;
+        if (timeRangeStr != null && !timeRangeStr.isEmpty()) {
+            timeRange = Arrays.stream(timeRangeStr.split(","))
+                    .map(DateTime::parse)
+                    .collect(Collectors.toList());
+        }
+
+        List<Integer> ageRange = null;
+        if (ageRangeStr != null && !ageRangeStr.isEmpty()) {
+            ageRange = Arrays.stream(ageRangeStr.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+
+        StringBuilder sb = new StringBuilder("Attractions filtered by: \n");
+        if (districts != null) sb.append("Districts: ").append(districts).append("\n");
+        if (ageRange != null) sb.append("Age Range: ").append(ageRange).append("\n");
+        if (gender != null) sb.append("Gender: ").append(gender).append("\n");
+        if (timeRange != null) sb.append("Time Range: ").append(timeRange).append("\n");
+
+        return sb.toString();
+    }
+
+    @GetMapping("/visits")
+    public String searchEvents(
+            @RequestParam(value = "districts", required = false) String districtStr,
+            @RequestParam(value = "gender", required = false) String gender,
+            @RequestParam(value = "ageRange", required = false) String ageRangeStr,
+            @RequestParam(value = "timeRange", required = false) String timeRangeStr) {
+
+        List<Integer> districts = null;
+        if (districtStr != null && !districtStr.isEmpty()) {
+            districts = Arrays.stream(districtStr.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+
+        List<DateTime> timeRange = null;
+        if (timeRangeStr != null && !timeRangeStr.isEmpty()) {
+            timeRange = Arrays.stream(timeRangeStr.split(","))
+                    .map(DateTime::parse)
+                    .collect(Collectors.toList());
+        }
+
+        List<Integer> ageRange = null;
+        if (ageRangeStr != null && !ageRangeStr.isEmpty()) {
+            ageRange = Arrays.stream(ageRangeStr.split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+
+        StringBuilder sb = new StringBuilder("Attractions filtered by: \n");
+        if (districts != null) sb.append("Districts: ").append(districts).append("\n");
+        if (ageRange != null) sb.append("Age Range: ").append(ageRange).append("\n");
+        if (gender != null) sb.append("Gender: ").append(gender).append("\n");
+        if (timeRange != null) sb.append("Time Range: ").append(timeRange).append("\n");
+
+        return sb.toString();
+    }
+}
